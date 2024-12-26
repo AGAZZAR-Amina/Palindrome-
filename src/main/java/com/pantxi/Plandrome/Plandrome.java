49415a0b6a7db456d3e822eb6f26e226c84e0e48
AGAZZAR-Amina <aminaagazzar7@gmail.com>
2024-12-26 11:38:30 +0100

    test pour une chaine vide

--- src/main/java/com/pantxi/Plandrome/Plandrome.java
+++ src/main/java/com/pantxi/Plandrome/Plandrome.java
@@ -1,5 +1,9 @@
 package com.pantxi.Plandrome;
 
 public class Plandrome {
-    
+
+    public static String IsPlandrome(String chaine){
+         chaine = "" ;
+        return "true"  ;
+    }
 }
--- src/test/java/com/pantxi/Plandrome/TestPlandrome.java
+++ src/test/java/com/pantxi/Plandrome/TestPlandrome.java
@@ -1,4 +1,14 @@
 package com.pantxi.Plandrome;
 
+import org.junit.jupiter.api.* ;
+import static org.junit.jupiter.api.Assertions.* ;
+import java.util.* ;
+
 public class TestPlandrome {
+    @Test
+    public void TestPlindrome1(){
+
+        assertEquals("true",Plandrome.IsPlandrome("")) ;
+    }
+
 }
